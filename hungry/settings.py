f8609718031ee6d47b1c77009931b921534187ce
bj@ubuntu.(none) <bj@ubuntu.(none)>
2011-03-22 20:01:05 -0700

    edited stylesheet and index

---
+++ hungry/settings.py
@@ -0,0 +1,157 @@
+import os
+
+ROOT_PATH = os.path.dirname(__file__)
+
+# DEBUG Settings
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# Site Administrators
+ADMINS = (
+     ('Trent Strong', 'trent@hungerstatus.com'),
+)
+
+MANAGERS = ADMINS
+
+# Database Backend
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'hungerstatus',                      # Or path to database file if using sqlite3.
+        'USER': 'hungerstatus',                      # Not used with sqlite3.
+        'PASSWORD': 'ttt4283RT',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Los_Angeles'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+# Site Identifier (to use in functionality specific to a web server instance)
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = ''
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (
+    os.path.join(ROOT_PATH, 'static'),
+)
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+
+SECRET_KEY = '$x^@9*3tae4siabuj5#q3wtogal_sxz1j@+f6*1*4@ws$7v&vp'
+
+# Amazon Web Services Keys
+
+AWS_KEY = 'AKIAIGOXVOZUZEUSXAGQ'
+AWS_SECRET = '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'
+
+# Transloadit Related
+
+TRANSLOADIT_KEY = '6311ca78f89642a1a4d2ae70fceacf57'
+TRANSLOADIT_SECRET = '870488d461d0706cefc9ee90e5bab6b68df1411b'
+TRANSLOADIT_URL = 'http://api2.transloadit.com/assemblies'
+TRANSLOADIT_TEMPLATES = {
+    'profile_photo': 'a6010eb2b85f46d5b25ba719a49f855b',
+}
+
+# List of callables that know how to import templates from various sources.
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+# Installed Middlware
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+# Master URL module
+
+ROOT_URLCONF = 'hungry.urls'
+
+# Search directories for templates
+
+
+TEMPLATE_DIRS = (
+    os.path.join(ROOT_PATH,'templates'),
+)
+
+# Registered Django Applications
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'hungry.accounts',
+    'registration',
+    'debug_toolbar',
+    'hungry.media',
+    # Uncomment the next line to enable the admin:
+    # 'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
+)
+
+# Email Backend Configuration
+if (DEBUG):
+    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+    EMAIL_FILE_PATH = '/home/trent/Development/hungerstatus/hungry/tmp/mail' # change this to a proper location
+
+# APPLICATION SPECIFIC SETTINGS
+
+# Registration
+
+ACCOUNT_ACTIVATION_DAYS = 7
+
+AUTH_PROFILE_MODULE = 'accounts.UserProfile'
+
+# Django Debug Toolbar
+
+INTERNAL_IPS = ('127.0.0.1',)
+
